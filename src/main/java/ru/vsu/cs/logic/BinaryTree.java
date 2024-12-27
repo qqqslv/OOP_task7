ba4797b69f97dabf40e6019a55168bb0d23103ff
ytutq <qqqslv@gmail.com>
2024-12-27 23:56:08 +0300

    added BinaryTree delete-method, but without findHeir method that is used in delete-method

--- src/main/java/ru/vsu/cs/logic/BinaryTree.java
+++ src/main/java/ru/vsu/cs/logic/BinaryTree.java
@@ -34,4 +34,63 @@ public class BinaryTree <T extends Comparable<T>> {
             }
         }
     }
+
+    private void delete(T value) {
+        Node<T> current = root;
+        Node<T> parent = root;
+        boolean isLeft = true;
+
+        while (value.compareTo(current.value) != 0) {
+            parent = current;
+            if (value.compareTo(current.value) < 0) {
+                isLeft = true;
+                current = current.left;
+            } else {
+                isLeft = false;
+                current = current.right;
+            }
+            if (current == null) {
+                return;
+            }
+        }
+
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeft) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            } else if (isLeft) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        } else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            } else if (isLeft) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        } else {
+            Node<T> heir = findHeir(current);
+            if (current == root) {
+                root = heir;
+            } else if (isLeft) {
+                parent.left = heir;
+            } else {
+                parent.right = heir;
+            }
+        }
+    }
+
+    private Node<T> findHeir(Node<T> node) {
+        return node; //not ready yet..((
+    }
 }
